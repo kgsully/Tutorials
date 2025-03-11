2d11aafa94474cfd659fa428aaafb2f07b4c85c5
Ken Sully <ken.sully@gmail.com>
2025-03-11 13:30:08 -0600

    commit to allow for updating angular cli/core

--- angular_a-z/wishlist/src/app/app.component.ts
+++ angular_a-z/wishlist/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'wishlist';
+
+  items: WishItem[] = [
+    new WishItem('To Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Find grass that cuts itself')
+  ];
 }
