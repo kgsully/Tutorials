e2310696dc8cfdbce80e296a84c4cc48cbf75694
Ken Sully <ken.sully@gmail.com>
2025-03-17 15:08:36 -0600

    Angular A-Z: Continuing tutorial - Added 'not found' route and wildcard entry in routing table to catch any non defined routes to display the message

--- angular_a-z/router-app/src/app/app.component.ts
+++ angular_a-z/router-app/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// SEE app.routes.ts FOR MORE INFORMATION - THIS IS A BASIC APPLICATION TO ILLUSTRATE ANGULAR ROUTER USAGE
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
--- angular_a-z/router-app/src/app/app.routes.ts
+++ angular_a-z/router-app/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     // NOTE! No leading '/' required on the paths
     // ALSO! If you specify a blank string for path, that is essentially the same as '/' which indicates the home page
-    // { path: '', component: FirstComponent},
+    // THE ROUTING TABLE ORDER MATTERS! ANGULAR USES A FIRST-MATCH POLICY AND STARTS WITH THE FIRST ELEMENT IN THE ROUTING TABLE ARRAY
+    { path: '', component: FirstComponent},
     { path: 'first', component: FirstComponent},
     { path: 'second', component: SecondComponent},
+    // The double asterisk '**' is a wildcard. Basically a catch all for anything that is not otherwise specified
+    { path: '**', component: NotFoundComponent},
 ];  // localhost:4200/<path> -> e.g. localhost:4200/first
