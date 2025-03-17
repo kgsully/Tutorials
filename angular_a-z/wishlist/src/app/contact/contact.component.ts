1805610e18f46d6b34a5464f2ae4ff508ca48ac1
Ken Sully <ken.sully@gmail.com>
2025-03-17 13:35:56 -0600

    Angular A-Z: Continuing tutorial - Updated contact reactive form to use a formGroup for input fields

--- angular_a-z/wishlist/src/app/contact/contact.component.ts
+++ angular_a-z/wishlist/src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormControl } from '@angular/forms'; // Using ReactiveFormsModules instead of FormsModule
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms'; // Using ReactiveFormsModules instead of FormsModule
 
 @Component({
   selector: 'app-contact',
@@ -20,16 +20,36 @@ export class ContactComponent {
     // Touched is another property that indicates whether the user has given focus to the form field and then took focus away
     //    this.senderMessageControl.touched
     // Perhaps the most useful will be the ability to use very expressive and flexible validators
+  // Additionally, we get a sense of data integrity, as the user submits a form, we get a snapshot of the data of that form at the time it was submitted.
+  // As such, don't have to worry about data being changed after form submittal (which differs from the template driven form approach)
 
+  // FormGroup used to group related form items. Accepts the argument of an object which allows us to define the data model
+  // It allows us to receive an object of the same type as the data model upon user submittal, and also allows us to check the validity of data from the form all at once
+  contactForm = new FormGroup({
+    senderName: new FormControl(''),
+    senderEmail: new FormControl(''),
+    senderMessage: new FormControl(''),
+  })
 
-  // The first argument sets the inital value. Setting it here to an empty string
-  senderNameControl = new FormControl('');
-  senderEmailControl = new FormControl('');
-  senderMessageControl = new FormControl('');
 
   submitForm() {
-    if(this.senderNameControl.dirty) {
-      alert('You changed the name field');
-    }
+    console.log(this.contactForm.value);
+    console.log(this.contactForm.valid);
   }
 }
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Original code before refactor
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Defined properties prior to creating the form group
+// The first argument sets the inital value. Setting it here to an empty string
+// senderNameControl = new FormControl('');
+// senderEmailControl = new FormControl('');
+// senderMessageControl = new FormControl('');
+
+// submitForm() {
+//   if(this.senderNameControl.dirty) {
+//     alert('You changed the name field');
+//   }
+// }
